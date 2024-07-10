ba5b340041be4788b76c6e5de8acecf2c40566f2
Ash Ketchum <727722eucs169@skcet.ac.in>
2024-07-10 23:27:01 +0530

    😍

--- BackEnd/Server/Server.js
+++ BackEnd/Server/Server.js
@@ -2,20 +2,32 @@ import express from "express"
 import dotenv from "dotenv"
 import cors from "cors"
 import bodyParser from "body-parser"
+import path, { dirname } from "path"
 import { ConnectToDB } from "../Database/DataBaseConnection.js";
 import Routes from "../Routes/Routes.js"
 
 
 const app = express();
+const _dirname = path.resolve()
 
 dotenv.config();
 const PORT = process.env.PORT || 5000
 
+app.use(express.static(path.join(_dirname,"FrontEnd/build")))
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
-app.use(cors({origin:"http://localhost:3000"}));
 app.use(express.json())
 app.use(Routes);
+
+
+
+app.use(cors({origin:"http://localhost:3000"}));
+
+app.get("*",(req,res)=>{
+
+    res.sendFile(path.join(_dirname,"FrontEnd","build","index.html"));
+
+})
 
 app.listen( PORT ,() =>{
     {
